718a8af464d0bf032fa02cf364dbf55ac8c6dbd1
SafariOl <s.olha.vit@gmail.com>
2024-08-23 14:12:11 +0300

    task 2 updated

--- task_2/main.py
+++ task_2/main.py
@@ -1,5 +1,13 @@
+N = 30
+
 sum = 0
+count = 0
+
 print("Число", " ", "Сума")
-for val in range(1, 31): 
-    sum += 1/val
-    print(val, " " * 5, sum)
+for i in range(1, N + 1):
+    sum += 1 / i
+    count += 1
+    print(i, " " * 5, sum)
+
+print(f"Сума членів ряду S для N={N}: {sum:.4f}")
+print(f"Кількість членів ряду S для N={N}: {count}")
